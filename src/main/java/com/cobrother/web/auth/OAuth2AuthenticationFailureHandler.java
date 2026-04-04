c7adc7aa35bceac91d08023300bd47d07f619a56
Ameerlog <aultum.com@gmail.com>
2026-04-04 10:59:01 +0530

    fix

--- src/main/java/com/cobrother/web/auth/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/cobrother/web/auth/OAuth2AuthenticationFailureHandler.java
@@ -3,6 +3,7 @@ package com.cobrother.web.auth;
 import com.cobrother.web.auth.util.HttpCookieOAuth2AuthorizationRequestRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
